383b892ae5d0f854ea8b527c41ab1d3834c713e6
Nourhan992 <98842557+Naeldes92@users.noreply.github.com>
2023-12-01 10:53:26 +0100

    updated the array

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function searchCity(city) {
 function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
 
     if (minutes < 10) {
