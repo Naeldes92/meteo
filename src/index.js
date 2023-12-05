9c86e115025d685338598c4c88e91078e7e832d0
Nourhan992 <98842557+Naeldes92@users.noreply.github.com>
2023-12-05 19:53:06 +0100

    Added a new function and changed CSS to make it more readable

--- src/index.js
+++ src/index.js
@@ -46,6 +46,13 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "b284bdab0tb6f48o74084a66cb9d2433";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
@@ -56,28 +63,26 @@ function showForecast(response) {
     console.log(response.data); 
 
 
-
-
-
-
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thur"]; 
     let forecastHtml = "";
 
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if(index < 5) {
         forecastHtml = 
          forecastHtml + 
          `
     <div class="weather-forecast-day">
-     <div class="weather-forecast-date">${day}</div>
-     <div class="weather-forecast-icon">⛅</div>
+     <div class="weather-forecast-date">${formatDay(day.time)}</div>
+     
+     <img src="${day.condition.icon_url}"  class="weather-forecast-icon"/>
      <div class="weather-forecast-temperatures">
         <div class="weather-forecast-temp">
-            <strong>10°</strong>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
         </div>
-        <div class="weather-forecast-temp">6°</div>
+        <div class="weather-forecast-temp">${Math.round(day.temperature.minimum)}°</div>
     </div>
   </div>
 `;
+}
 });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
